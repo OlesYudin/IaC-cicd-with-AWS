06b767e2c57c97cc0b8cdc22bc1611d1e0c08a2b
Oles Yudin <o.yudin>
2022-03-31 21:28:01 +0300

    Test SNS

--- app/htdocs/index.php
+++ app/htdocs/index.php
@@ -13,6 +13,7 @@
 		<header>
 			<div class="title">
 				<h1>Generation of pseudo-random passwords!</h1>
+				<h1>Generation of pseudo-random passwords!</h1>
 				<p>
 				Generation of pseudo-random passwords based on entropy operating system based on the Linux kernel
 				</p>
