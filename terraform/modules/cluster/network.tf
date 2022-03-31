bc1f8fb9d054f65bd91ffcf9fe3524ffd26b7277
Oles Yudin <o.yudin>
2022-03-31 23:59:55 +0300

    Add private network for cluster

--- terraform/modules/cluster/network.tf
+++ terraform/modules/cluster/network.tf
@@ -65,63 +65,63 @@ resource "aws_route_table_association" "publicrouteAssociation" {
 }
 
 
-# # Resources for Private Subnets
-# # Add Elastic IP
-# # This block create EIP and attach it to VPC
-# # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
-# resource "aws_eip" "nat_eip" {
-#   vpc = true # Use, if EIP should be in VPC
-#   tags = {
-#     Name = "EIP-${var.env}"
-#   }
-# }
+# Resources for Private Subnets
+# Add Elastic IP
+# This block create EIP and attach it to VPC
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
+resource "aws_eip" "nat_eip" {
+  vpc = true # Use, if EIP should be in VPC
+  tags = {
+    Name = "EIP-${var.env}"
+  }
+}
 
-# # Create Network Address Translation
-# # This block create 1 NAT in 1 public subnet and attach public IP-address
-# # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/nat_gateway
-# resource "aws_nat_gateway" "nat" {
-#   allocation_id = aws_eip.nat_eip.id             # Attach to Elastic IP
-#   subnet_id     = aws_subnet.public_subnet[0].id # In what subnet will be NAT
+# Create Network Address Translation
+# This block create 1 NAT in 1 public subnet and attach public IP-address
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/nat_gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id             # Attach to Elastic IP
+  subnet_id     = aws_subnet.public_subnet[0].id # In what subnet will be NAT
 
-#   tags = {
-#     Name        = "NAT-GW-${var.env}"
-#     Environment = var.env
-#     AZ          = "${data.aws_availability_zones.available.names[0]}"
-#   }
-# }
+  tags = {
+    Name        = "NAT-GW-${var.env}"
+    Environment = var.env
+    AZ          = "${data.aws_availability_zones.available.names[0]}"
+  }
+}
 
-# # Private Subnet
-# # This block create 2 privare subnets in 2 AZ
-# resource "aws_subnet" "private_subnet" {
-#   count             = length(var.private_subnet)
-#   vpc_id            = aws_vpc.vpc.id
-#   cidr_block        = var.private_subnet[count.index]
-#   availability_zone = data.aws_availability_zones.available.names[count.index]
+# Private Subnet
+# This block create 2 privare subnets in 2 AZ
+resource "aws_subnet" "private_subnet" {
+  count             = length(var.private_subnet)
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private_subnet[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 
-#   tags = {
-#     Name        = "Private-Subnet-${var.env}-${count.index + 1}"
-#     Environment = "${var.env}"
-#     AZ          = "${data.aws_availability_zones.available.names[count.index]}"
-#   }
-# }
+  tags = {
+    Name        = "Private-Subnet-${var.env}-${count.index + 1}"
+    Environment = "${var.env}"
+    AZ          = "${data.aws_availability_zones.available.names[count.index]}"
+  }
+}
 
-# # Route table from Internet to private subnets 
-# resource "aws_route_table" "privateroute" {
-#   vpc_id = aws_vpc.vpc.id
+# Route table from Internet to private subnets 
+resource "aws_route_table" "privateroute" {
+  vpc_id = aws_vpc.vpc.id
 
-#   route {
-#     cidr_block     = var.default_cidr       # Allow all to IN/OUT traffic
-#     nat_gateway_id = aws_nat_gateway.nat.id # Attach to NAT
-#   }
+  route {
+    cidr_block     = var.default_cidr       # Allow all to IN/OUT traffic
+    nat_gateway_id = aws_nat_gateway.nat.id # Attach to NAT
+  }
 
-#   tags = {
-#     Name        = "PrivateRouteTable-${var.env}"
-#     Environment = var.env
-#   }
-# }
+  tags = {
+    Name        = "PrivateRouteTable-${var.env}"
+    Environment = var.env
+  }
+}
 
-# resource "aws_route_table_association" "privaterouteAssociation" {
-#   count          = length(var.private_subnet)
-#   subnet_id      = aws_subnet.private_subnet[count.index].id
-#   route_table_id = aws_route_table.privateroute.id
-# }
+resource "aws_route_table_association" "privaterouteAssociation" {
+  count          = length(var.private_subnet)
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.privateroute.id
+}
--- terraform/modules/cluster/outputs.tf
+++ terraform/modules/cluster/outputs.tf
@@ -2,18 +2,22 @@
 output "vpc_ip" {
   value = aws_vpc.vpc.cidr_block
 }
-# # Output Public Elastic IP
-# output "eip_public_ip" {
-#   value = aws_eip.nat_eip.public_ip
-# }
+# Output VPC id
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
+# Ouput subnets id
+output "subnets_id" {
+  value = aws_subnet.private_subnet.*.id
+}
+# Output Public Elastic IP
+output "eip_public_ip" {
+  value = aws_eip.nat_eip.public_ip
+}
 # Output DNS name of ALB
 output "alb_dns" {
   value = aws_lb.alb.dns_name
 }
-# # Ouput url of s3 bucket for tf_state
-# output "s3_url_tf_state" {
-#   value = aws_s3_bucket.terraform_state.bucket_regional_domain_name
-# }
 # Output name of task definition
 output "task_definition_family" {
   value = aws_ecs_task_definition.task-definition.family
