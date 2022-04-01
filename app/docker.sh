5664bb0be3df05741919bce3cfa2a319e0fe3a4b
Oles Yudin <o.yudin>
2022-04-01 11:54:29 +0300

    Add ecr to init build

--- app/docker.sh
+++ app/docker.sh
@@ -4,8 +4,8 @@
 sudo aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.${region}.amazonaws.com
 
 # Build container
-docker build -t ${account_id}.dkr.ecr.${region}.amazonaws.com/${app_name}:latest .
-docker build -t ${account_id}.dkr.ecr.${region}.amazonaws.com/${app_name}:${image_tag} .
+docker build -t ${account_id}.dkr.ecr.${region}.amazonaws.com/${registry_url}:latest .
+docker build -t ${account_id}.dkr.ecr.${region}.amazonaws.com/${registry_url}:${image_tag} .
 
 # Push container to AWS
 docker push ${account_id}.dkr.ecr.${region}.amazonaws.com/${registry_url}:latest
