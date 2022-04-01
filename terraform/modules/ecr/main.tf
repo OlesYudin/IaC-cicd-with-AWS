a4176a301bf12b9c8c945c7f45d11b3c8d9ac1ec
Oles Yudin <o.yudin>
2022-04-01 11:45:08 +0300

    Change ECR repository for 2 env

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,6 +1,6 @@
 # Create ECR repository
 resource "aws_ecr_repository" "ecr_repository" {
-  name = var.app_name
+  name = "${var.app_name}-${var.env}-repository"
   #   image_tag_mutability = "IMMUTABLE" # If immutable you cant write to same tag in 1 repo
 
   image_scanning_configuration {
@@ -35,6 +35,6 @@ resource "aws_ecr_lifecycle_policy" "test_lifecycle_policy" {
 
 # Output information about ECR repository
 data "aws_ecr_repository" "ecr_repository" {
-  name       = var.app_name # Get name of ECR repo for output info about this repository
+  name       = "${var.app_name}-${var.env}-repository" # Get name of ECR repo for output info about this repository
   depends_on = [aws_ecr_repository.ecr_repository]
 }
