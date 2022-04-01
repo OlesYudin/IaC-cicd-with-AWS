6e530154558b6ab1a80e17688715e8926b0f0699
Oles Yudin <o.yudin>
2022-04-01 11:33:52 +0300

    Remove test

--- app/htdocs/index.php
+++ app/htdocs/index.php
@@ -13,7 +13,6 @@
 		<header>
 			<div class="title">
 				<h1>Generation of pseudo-random passwords!</h1>
-				<h1>Generation of pseudo-random passwords!</h1>
 				<p>
 				Generation of pseudo-random passwords based on entropy operating system based on the Linux kernel
 				</p>
